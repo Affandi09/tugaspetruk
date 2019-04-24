3acf3c9232e860d05d35fb8a26f293a2604ba37a
Affandi09 <48713631+Affandi09@users.noreply.github.com>
2019-04-25 00:09:57 +0700

    Update infix.cpp

--- infix.cpp
+++ infix.cpp
@@ -1,1 +1,55 @@
+#include <iostream>
+#define n 100
+using namespace std;
+
+
+	class Stack
+	{
+	private:
+		int data[n];
+		int top;
+	public:
+		Stack()
+		{
+			top = -1;
+		}
+
+		int Top() 
+		{
+			return data[top];
+		}
+
+		int IsEmpty()
+		{
+			if(top == -1) return 1;
+			else return 0;
+		}
+		void Push(int x) 
+		{
+		  if(top == n-1) {
+				cout<<"Stack Penuh";
+			}
+			else {data[++top] = x;
+			}
+		}
+
+
+		void Pop() 
+		{
+			if(top == -1) {
+				cout<<"Stack Kosong";
+			}
+			else {top--;}	
+		} 
+
+		void removespace ()
+		{
+			for(int i=0;data[i];i++){
+			while(data[i] == ' '){
+				for(int j=i;data[j];j++){
+					data[j] = data[j+1];
+					}
+					}
+				}
+		}
 
